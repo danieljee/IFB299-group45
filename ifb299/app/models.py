17724ea6af7db1eb7c56cde0ce22c9e32a4cce20
Daniel <danieljee123@gmail.com>
2017-09-12 23:24:43 +1000

    changed model

--- ifb299/app/models.py
+++ ifb299/app/models.py
@@ -26,7 +26,7 @@ class User(models.Model):
     lastName = models.CharField(max_length=100)
     emailAddress = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
-    address = models.CharField(max_length=200)
+    address = models.CharField(max_length=100)
     postcode = models.IntegerField()
     role = models.CharField(
         max_length=2,
