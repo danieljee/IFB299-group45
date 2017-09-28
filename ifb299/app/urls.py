d7765de472498175936ebd4a5a3a1c9c84f826da
andrewgrant14029 <30451899+andrewgrant14029@users.noreply.github.com>
2017-09-28 19:01:18 +1000

    Update urls.py

--- ifb299/app/urls.py
+++ ifb299/app/urls.py
@@ -12,7 +12,8 @@ urlpatterns = [
     url(r'^account/loggedIn$', views.loggedIn, name='loggedIn'),
     url(r'^account/logout$', views.logout, name='logout'),
     url(r'^account/invalid$', views.invalidLogin, name='invalid'),
-    	url(r'^account/information$', views.AccountInformation.as_view(), name='account'),
+    url(r'^account/information$', views.AccountInformation.as_view(), name='account'),
+    url(r'^account/edit$', views.EditAccountInformation.as_view(), name='edit_account'),
 
     url(r'^search/$', views.Search.as_view(), name='search'),
     url(r'^search/(?P<order>)/$', views.search_ordered, name='search_ordered'),
