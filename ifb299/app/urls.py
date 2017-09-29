761dea29d3a4c38104b8c1924f9a4098f3d02a5c
sandrafinow <sfinow@hotmail.com>
2017-09-29 19:04:12 +1000

    Added category result pages

--- ifb299/app/urls.py
+++ ifb299/app/urls.py
@@ -16,5 +16,15 @@ urlpatterns = [
 
     url(r'^search/$', views.Search.as_view(), name='search'),
     url(r'^search/(?P<order>)/$', views.search_ordered, name='search_ordered'),
-    url(r'^place/(?P<pk>\d+)/$', views.PlaceDetail.as_view(), name='place_detail')
+    url(r'^place/(?P<pk>\d+)/$', views.PlaceDetail.as_view(), name='place_detail'),
+    
+    	url(r'^restaurants/$', views.RestaurantCategory.as_view(), name ='restaurant'),
+	url(r'^malls/$', views.MallCategory.as_view(), name ='malls'),
+	url(r'^industries/$', views.IndustryCategory.as_view(), name ='industries'),
+	url(r'^colleges/$', views.CollegeCategory.as_view(), name ='colleges'),
+	url(r'^zoos/$', views.ZooCategory.as_view(), name ='zoos'),
+	url(r'^hotels/$', views.HotelCategory.as_view(), name ='hotels'),
+	url(r'^museums/$', views.MuseumCategory.as_view(), name ='museums'),
+	url(r'^parks/$', views.ParkCategory.as_view(), name ='parks'),
+	url(r'^libraries/$', views.LibraryCategory.as_view(), name ='libraries'),
 ]
