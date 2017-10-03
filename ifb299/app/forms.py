52e298ec556a3bb450dd0c2b948c5c9cf697feeb
andrewgrant14029 <30451899+andrewgrant14029@users.noreply.github.com>
2017-10-03 17:21:42 +1000

    Update forms.py
    Added for for editing user information

--- ifb299/app/forms.py
+++ ifb299/app/forms.py
@@ -26,3 +26,11 @@ class UserProfileForm(forms.ModelForm):
             'postcode': forms.NumberInput(attrs={'class':'form-control'}),
             'role': forms.Select(attrs={'class':'form-control'}),
         }
+        
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=30, required=False)
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name')
