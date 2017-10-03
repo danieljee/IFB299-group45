72879ad284330453d9ad52405d1bf4c49726d7a2
aidenater1998 <30451599+aidenater1998@users.noreply.github.com>
2017-10-03 17:49:17 +1000

    Update admin.py

--- ifb299/app/admin.py
+++ ifb299/app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile, City, Place, State, Category, Department, Industry
+from .models import UserProfile, City, Place, SavedPlace, State, Category, Department, Industry
 
 class PlaceAdmin(admin.ModelAdmin):
     filter_horizontal = ('department',)
@@ -8,7 +8,9 @@ admin.site.register(UserProfile)
 admin.site.register(Category)
 admin.site.register(City)
 admin.site.register(Place, PlaceAdmin)
+admin.site.register(SavedPlace)
 admin.site.register(State)
 admin.site.register(Department)
 admin.site.register(Industry)
+
 # Register your models here.
