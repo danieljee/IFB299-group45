2fb0e7f158438c8650e02b5596aa55bf4dd10cc5
sandrafinow <sfinow@hotmail.com>
2017-10-03 13:50:50 +1000

    Updated

--- ifb299/app/views.py
+++ ifb299/app/views.py
@@ -1,24 +1,43 @@
 from django.shortcuts import render_to_response, render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.views import generic
 from django.contrib import auth
+from django.contrib.auth.models import User
+from .models import UserProfile, Place
 from django.contrib.auth import authenticate, login
 from django.template.context_processors import csrf
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from .forms import MyRegistrationForm, UserProfileForm
-
 
 from .models import Place
 
+class HttpResponseUnauthorized(HttpResponseRedirect):
+    status_code = 401
+
+class HttpResponseNotFound(HttpResponseRedirect):
+    status_code = 404
+
+#def index(request):
+#    if request.method == 'GET':
+#        args = {}
+#        args.update(csrf(request))
+#        args['user_form'] = MyRegistrationForm()
+#        args['profile_form'] = UserProfileForm()
+#        return render(request, 'index.html', args)
+
 def index(request):
-    if request.method == 'GET':
-        args = {}
-        args.update(csrf(request))
-        args['user_form'] = MyRegistrationForm()
-        args['profile_form'] = UserProfileForm()
-        return render(request, 'index.html', args)
-
+	if request.method == 'GET':
+		args = {}
+		args.update(csrf(request))
+		args['user_form'] = MyRegistrationForm()
+		args['profile_form'] = UserProfileForm()
+		if request.user.is_authenticated() and not request.user.is_superuser:
+			profile = UserProfile.objects.filter(user=request.user).first()
+			args['role'] = profile.role
+		return render(request, 'index.html', args)
+	
 def register_user(request):
-
     if request.method == 'POST':
         user_form = MyRegistrationForm(data = request.POST)
         profile_form = UserProfileForm(data = request.POST)
@@ -41,36 +60,30 @@ def register_user(request):
 
 def register_success(request):
     if request.user.is_authenticated():
-        print('user is authenticated')
-        return HttpResponseRedirect('/')
+        return HttpResponseNotFound('/')
     return render(request, 'register_success.html')
 
 
 def auth_view(request):
     if request.user.is_authenticated():
-        return HttpResponseRedirect('/')
+        return HttpResponseNotFound('/')
     username = request.POST.get('username', '')
     password = request.POST.get('password', '')
     user = auth.authenticate(username=username, password=password)
     if user is not None:
         auth.login(request, user)
-        return HttpResponseRedirect('/account/loggedIn')
+        return HttpResponseRedirect('/')
     else:
         return HttpResponseRedirect('/account/invalid')
 
-def loggedIn(request):
-    if request.user.is_authenticated():
-        return HttpResponseRedirect('/')
-    return render(request, 'loggedIn.html', {'username': request.user.username})
-
 def invalidLogin(request):
     if request.user.is_authenticated():
-        return HttpResponseRedirect('/')
+        return HttpResponseNotFound('/')
     return render(request, 'invalidLogin.html')
 
 def logout(request):
     if not request.user.is_authenticated():
-        return HttpResponseRedirect('/')
+        return HttpResponseNotFound('/')
     auth.logout(request)
     return render(request, 'logout.html')
 
@@ -84,6 +97,11 @@ class Search(generic.ListView):
     template_name = 'search.html'
     context_object_name = 'place_list'
     paginate_by = 10
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(Search, self).dispatch(*args, **kwargs)
+
     def get_queryset(self):
         self.query = self.request.GET.get('q')
         return Place.objects.filter(name__icontains=self.query)
@@ -97,3 +115,74 @@ class PlaceDetail(generic.DetailView):
     model = Place
     template_name = 'placeDetail.html'
     context_object_name = 'place'
+    
+class AccountInformation(generic.ListView):
+    model = UserProfile
+    template_name = 'AccountInformation.html'
+    context_object_name = 'users'
+    def get_queryset(self):
+        return UserProfile.objects.filter(user=self.request.user)
+    
+class EditAccountInformation(generic.ListView):
+    model = UserProfile
+    template_name = 'EditAccount.html'
+    context_object_name = 'users'
+    def get_queryset(self):
+        return UserProfile.objects.filter(user=self.request.user)
+
+class RestaurantCategory(generic.ListView):
+	model = Place
+	template_name = 'restaurants.html'
+	context_object_name = 'place_list'
+	
+class IndustryCategory(generic.ListView):
+	model = Place
+	template_name = 'industries.html'
+	context_object_name = 'place_list'
+	
+class MallCategory(generic.ListView):
+	model = Place
+	template_name = 'malls.html'
+	context_object_name = 'place_list'
+
+class LibraryCategory(generic.ListView):
+	model = Place
+	template_name = 'libraries.html'
+	context_object_name = 'place_list'
+
+class ZooCategory(generic.ListView):
+	model = Place
+	template_name = 'zoos.html'
+	context_object_name = 'place_list'
+	
+class CollegeCategory(generic.ListView):
+	model = Place
+	template_name = 'colleges.html'
+	context_object_name = 'place_list'
+	
+class MuseumCategory(generic.ListView):
+	model = Place
+	template_name = 'museums.html'
+	context_object_name = 'place_list'
+
+class HotelCategory(generic.ListView):
+	model = Place
+	template_name = 'hotels.html'
+	context_object_name = 'place_list'
+
+class ParkCategory(generic.ListView):
+	model = Place
+	template_name = 'parks.html'
+	context_object_name = 'place_list'
+	
+
+
+#class savedPlaces(generic.Listview):
+#    model = Saved Places
+#    template_name = 'savedPlaces.html'
+#    context_object_name = 'savedPlaces'
+
+#class individualItemPage(generic.DetailView):
+#    model = Individual Item
+#    template_name = 'individualItemPage.html'
+#    context_object_name = 'individualItemPage'
