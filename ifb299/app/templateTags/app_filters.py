501393d36cfb7508643848bc4b76e09be611b9ad
sandrafinow <sfinow@hotmail.com>
2017-10-15 02:24:23 +1000

    Updated for category_banners

--- ifb299/app/templateTags/app_filters.py
+++ ifb299/app/templateTags/app_filters.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter(name='category_image')
 def category_image(value):
     return value.lower() + '.png'
+
+@register.filter(name='category_banner')
+def category_banner(value):
+	return value.lower() + '_banner1.png'
