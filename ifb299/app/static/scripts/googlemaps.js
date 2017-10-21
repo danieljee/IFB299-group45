ee859d609532ab756bd710f2b9bfa622eb2d12bf
Daniel <danieljee123@gmail.com>
2017-10-21 12:17:26 +1000

    weather api

--- ifb299/app/static/scripts/googlemaps.js
+++ ifb299/app/static/scripts/googlemaps.js
@@ -22,7 +22,7 @@ function initMap() {
       if (status == google.maps.GeocoderStatus.OK){
         if (status != google.maps.GeocoderStatus.ZERO_RESULTS){
           map.setCenter(results[0].geometry.location);
-
+          
           var infowindow = new google.maps.InfoWindow({
             content: '<b>' + address + '</b>',
             size: new google.maps.Size(150, 50)
@@ -37,6 +37,59 @@ function initMap() {
           google.maps.event.addListener(marker, 'click', function() {
             infowindow.open(map, marker);
           });
+
+          let lng = results[0].geometry.location.lng();
+          let lat = results[0].geometry.location.lat();
+
+          fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&APPID=709073822b9f373f5fba056d694642a6 `)
+          .then((res) => {
+            return res.json();
+          })
+          .then((resjson) => {
+            console.log(resjson);
+            let weatherDiv = document.getElementById('weatherDiv');
+            if (resjson.weather[0].main === "Rain"){
+              weatherDiv.innerHTML = `<h4 style="color:#fff; margin:0px; margin-top:10px;">Weather</h4>
+                                      <h5 style="color:#fff; margin:0px">Rainy</h5>
+                                      <div class="weatherIcon">
+                                        <div class="cloud"></div>
+                                        <div class="rain"></div>
+                                      </div>`;
+            } else if (resjson.weather[0].main === "Drizzle"){
+              weatherDiv.innerHTML = `<h4 style="color:#fff; margin:0px; margin-top:10px;">Weather</h4>
+                                      <h5 style="color:#fff; margin:0px">Drizzle</h5>
+                                      <div class="weatherIcon">
+                                        <div class="cloud"></div>
+                                        <div class="sun"><div class="rays"></div></div>
+                                        <div class="rain"></div>
+                                      </div>`;
+            }else if (resjson.weather[0].main === "Clear"){
+              weatherDiv.innerHTML = `<h4 style="color:#fff; margin:0px; margin-top:10px;">Weather</h4>
+                                      <h5 style="color:#fff; margin:0px">Clear</h5>
+                                      <div class="weatherIcon">
+                                        <div class="sun"><div class="rays"></div></div>
+                                      </div>`;
+            } else if (resjson.weather[0].main === "Clouds"){
+              weatherDiv.innerHTML = `<h4 style="color:#fff; margin:0px; margin-top:10px;">Weather</h4>
+                                       <h5 style="color:#fff; margin:0px">Cloudy</h5>
+                                      <div class="weatherIcon">
+                                        <div class="cloud"></div>
+                                        <div class="cloud"></div>
+                                      </div>`;
+            } else if (resjson.weather[0].main === "Thunderstorm"){
+              weatherDiv.innerHTML = `<h4 style="color:#fff; margin:0px; margin-top:10px;">Weather</h4>
+                                       <h5 style="color:#fff; margin:0px">Thunderstorm</h5>
+                                      <div class="weatherIcon">
+                                        <div class="cloud"></div>
+                                        <div class="lightning"><div class="bolt"></div><div class="bolt"></div></div>
+                                      </div>`;
+            }
+
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+
         } else {
           alert("Invalid address");
         }
